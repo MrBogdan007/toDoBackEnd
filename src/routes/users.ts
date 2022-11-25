10aa87b405f8c1b5eefdcaedff996b465bd18d03
Bohdan <bogdanbrohovetsky@gmail.com>
2022-11-26 00:28:31 +0200

    added token

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,22 @@
 import { pool } from "@src/databasePostgreSQL/db";
 import { Router } from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv'
+dotenv.config()
 
 const usersRoute = Router();
 
+//get a user
+usersRoute.get("/users", authenticateToken, (req:any, res:any) => {
+   console.log(req.users);
+   
+   // const users = await pool.query(
+   //    "SELECT email,password FROM users WHERE email = $1" ,
+   //    [req.users.rows[0]]
+});
+
+//registrate user 
 usersRoute.post("/signup", async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -19,6 +32,7 @@ usersRoute.post("/signup", async (req, res) => {
     });
   }
 });
+//authenticate user
 usersRoute.post("/signin", async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -29,8 +43,11 @@ usersRoute.post("/signin", async (req, res) => {
     if (users === null) {
       return res.status(400).send("Cannot find user");
     }
+    
     if (await bcrypt.compare(password, users.rows[0].password)) {
-      res.send("Success");
+    
+   const accessToken = jwt.sign(users.rows[0], process.env.ACCESS_TOKEN_SECRET as string);
+    res.json({ accessToken: accessToken})
     } else {
       res.send("Not Allowed");
     }
@@ -38,5 +55,19 @@ usersRoute.post("/signin", async (req, res) => {
     res.status(500).send();
   }
 });
+
+function authenticateToken(req:any, res:any, next:any) {
+   const authHeader = req.headers['authorization']
+   console.log(authHeader.split(' ')[1]);
+   
+   const token = authHeader && authHeader.split(' ')[1]
+   if (token == null) return res.sendStatus(401)
+
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err:any, users:any) => {
+      if (err) return res.sendStatus(403)
+      req.users = users
+      next()
+   })
+}
 
 export default usersRoute;
